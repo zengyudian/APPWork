469ed09dd9d3cf7907df8bac923b84ee7aede79b
zengyudian <zengyd@smail.swufe.edu.cn>
2020-10-09 19:50:57 +0800

    计数器

--- app/src/main/java/com/example/appwork/MainActivity.java
+++ app/src/main/java/com/example/appwork/MainActivity.java
@@ -5,40 +5,49 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText editText;
-    TextView out;
+    TextView tv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        out=findViewById(R.id.tv1);
-        //out.setText("123");
-
-
-        editText =(EditText)findViewById(R.id.editTextTextPersonName);
-        //String str=editText.getText().toString();
-        Button btn=(Button)findViewById(R.id.btn);
-        //add something...
-
-
+        tv=findViewById(R.id.textView2);
     }
 
-    public void abc(View v){
-
+    public void one(View v){
         Log.i("main","onClick msg");
-        //EditText editText =(EditText)findViewById(R.id.editTextTextPersonName);
-        String str=editText.getText().toString();
-        float i=Float.parseFloat(str);
-        float j=i*9/5+32;
-        String s=""+j;
-        out.setText(s);
-
+        String str=tv.getText().toString();
+        Integer i=Integer.parseInt(str);
+        i=i+1;
+        String j=""+i;
+        tv.setText(j);
     }
+
+    public void two(View v){
+        Log.i("main","onClick msg");
+        String str=tv.getText().toString();
+        Integer i=Integer.parseInt(str);
+        i=i+2;
+        String j=""+i;
+        tv.setText(j);
+    }
+
+    public void three(View v){
+        Log.i("main","onClick msg");
+        String str=tv.getText().toString();
+        Integer i=Integer.parseInt(str);
+        i=i+3;
+        String j=""+i;
+        tv.setText(j);
+    }
+
+    public void reset(View v){
+        Log.i("main","onClick msg");
+        tv.setText("0");
+    }
+
 }
