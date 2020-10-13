0155db3b9a3599bddf8d99bd4ffa86105199d6c1
zengyudian <zengyd@smail.swufe.edu.cn>
2020-10-13 11:03:05 +0800

    使用自定义列表显示汇率

--- app/src/main/java/com/example/appwork/RateListActivity.java
+++ app/src/main/java/com/example/appwork/RateListActivity.java
@@ -1,5 +1,6 @@
 package com.example.appwork;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.app.ListActivity;
 import android.content.SharedPreferences;
@@ -11,6 +12,7 @@ import android.preference.PreferenceManager;
 import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.ListAdapter;
+import android.widget.SimpleAdapter;
 
 import androidx.annotation.RequiresApi;
 
@@ -25,7 +27,11 @@ import java.time.LocalDate;
 import java.time.Period;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import static android.content.ContentValues.TAG;
 
@@ -89,15 +95,37 @@ public class RateListActivity extends ListActivity {
             //读取上次保存的数据
             SharedPreferences sp = getSharedPreferences("ratelist", Activity.MODE_PRIVATE);
             PreferenceManager.getDefaultSharedPreferences(this);
+            Log.i(TAG,"读取数据");
 
             String str1 = "";
-            str1 = sp.getString("list", "");
+            String str2="";
+            str1 = sp.getString("detaillist", "");
+            str2 = sp.getString("titlelist", "");
+            Log.i(TAG,"detail:"+str1);
+            Log.i(TAG,"title:"+str2);
             String[] str1Array = str1.split(",");
-            List<String> ratelist = Arrays.asList(str1Array);
+            String[] str2Array = str2.split(",");
 
-            ListAdapter adapter = new ArrayAdapter<String>(this,
+            List ratelist =new ArrayList<HashMap<String,String>>();
+            for(int i = 0;i <str1Array.length;i++)
+            {
+                HashMap<String,String> map=new HashMap<String,String>();
+                map.put("ItemTitle",str2Array[i]);//标题文字
+                map.put("ItemDetail",str1Array[i]);//详情描述
+                ratelist.add(map);
+            }
+
+
+            ListAdapter listItemAdapter = new SimpleAdapter(RateListActivity.this,
+                    ratelist, // listItems 数据源
+                    R.layout.activity_rate_list, // ListItem 的 XML 布局实现
+                    new String[] { "ItemTitle", "ItemDetail" },
+                    new int[] { R.id.itemTitle, R.id.itemDetail }
+            );
+            setListAdapter(listItemAdapter);
+            /*ListAdapter adapter = new ArrayAdapter<String>(this,
                     android.R.layout.simple_list_item_1, ratelist);
-            setListAdapter(adapter);
+            setListAdapter(adapter);*/
         }
 
         else {
@@ -107,7 +135,8 @@ public class RateListActivity extends ListActivity {
                 public void run() {
 
                     //获取网络数据
-                    List<String> rate_list =new ArrayList<String>();
+                    //List<String> name_list =new ArrayList<String>();
+                    List rate_list =new ArrayList<HashMap<String,String>>();
                     try {
                         String url1 = "http://www.usd-cny.com/bankofchina.htm";
                         Document doc = Jsoup.connect(url1).get();
@@ -123,16 +152,21 @@ public class RateListActivity extends ListActivity {
 
 
                         for (int i = 0; i < tds.size(); i += 6) {
+                            HashMap<String,String> map=new HashMap<String,String>();
                             Element td1 = tds.get(i);
                             Element td2 = tds.get(i + 5);
                             String str1 = td1.text();
                             String val = td2.text();
                             //Log.i(TAG, "run: " + str1 + "==>" + val);
-                            rate_list.add(str1 + "==>" + val);
+                            //rate_list.add(val);
+                            map.put("ItemTitle",str1);//标题文字
+                            map.put("ItemDetail",val);//详情描述
+                            rate_list.add(map);
+
 
                             // 获取数据并返回 ……
                         }
-                        Log.i(TAG, "run: " + rate_list);
+                        //Log.i(TAG, "run: " + rate_list);
 
 
                     } catch (MalformedURLException e) {
@@ -141,16 +175,24 @@ public class RateListActivity extends ListActivity {
                         e.printStackTrace();
                     }
 
+
                     //保存汇率数据
                     SharedPreferences sp=getSharedPreferences("ratelist", Activity.MODE_PRIVATE);
                     SharedPreferences.Editor editor=sp.edit();
 
-                    String str="";
-                    for(String s:rate_list){
-                        str+=s+",";
+                    String detailStr="";
+                    String titleStr="";
+                    for(int i = 0;i < rate_list.size();i++)
+                    {
+                        Map<String,String> map = (Map<String,String>) rate_list.get(i);
+                        String detail = map.get("ItemDetail").toString();
+                        String title = map.get("ItemTitle").toString();
+                        detailStr+=detail+",";
+                        titleStr+=title+",";
                     }
 
-                    editor.putString("list", str);
+                    editor.putString("detaillist", detailStr);
+                    editor.putString("titlelist", titleStr);
                     Log.i(TAG,"saveOK");
                     editor.apply();
 
@@ -184,12 +226,21 @@ public class RateListActivity extends ListActivity {
             @Override
             public void handleMessage(Message msg) {
                 if (msg.what == 5) {
-                    List<String> list2 = (List<String>) msg.obj;
-                    ListAdapter adapter = new ArrayAdapter<String>(
+                    List<HashMap<String,String>> list2 = (List<HashMap<String,String>>)msg.obj;
+                    Log.i(TAG,"sentOK");
+                    /*ListAdapter adapter = new ArrayAdapter<String>(
                             RateListActivity.this,
                             android.R.layout.simple_list_item_1,
                             list2);
-                    setListAdapter(adapter);
+                    setListAdapter(adapter);*/
+                    // 生成适配器的 Item 和动态数组对应的元素
+                    ListAdapter listItemAdapter = new SimpleAdapter(RateListActivity.this,
+                            list2, // listItems 数据源
+                            R.layout.activity_rate_list, // ListItem 的 XML 布局实现
+                            new String[] { "ItemTitle", "ItemDetail" },
+                            new int[] { R.id.itemTitle, R.id.itemDetail }
+                    );
+                    setListAdapter(listItemAdapter);
                 }
                 super.handleMessage(msg);
             }
